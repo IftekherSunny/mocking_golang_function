169a1d6c16b666602c93fd66e6f1d09be9cdaefc
Iftekher Sunny <iftekhersunny@hotmail.com>
2017-06-13 12:31:52 +0600

    First commit

---
+++ mocking.go
@@ -0,0 +1,17 @@
+package mocking_golang_function
+
+////////////////////////////////////////////
+// Define payment process type
+////////////////////////////////////////////
+type PaymentProcess func(amount float32) bool
+
+////////////////////////////////////////////
+//  Shopping cart function
+////////////////////////////////////////////
+func ShoppingCart(amount float32, paymentProcess PaymentProcess) string {
+	if paymentProcess(amount) {
+		return "Payment has been accepted"
+	}
+
+	return "Something wrong!!!"
+}
---
+++ mocking_test.go
@@ -0,0 +1,27 @@
+package mocking_golang_function
+
+import "testing"
+
+////////////////////////////////////////////
+// Mocking payment process function
+////////////////////////////////////////////
+func mockPaymentProcess(amount float32) bool {
+	if amount == 100 {
+		return true
+	}
+
+	return false
+}
+
+func TestShoppingCart(t *testing.T) {
+	t.Run("Testing shopping cart feature", func(t *testing.T) {
+
+		if ShoppingCart(100, mockPaymentProcess) != "Payment has been accepted" {
+			t.Error("Message must be 'Payment has been accepted'")
+		}
+
+		if ShoppingCart(0, mockPaymentProcess) != "Something wrong!!!" {
+			t.Error("Message must be 'Something wrong!!!'")
+		}
+	})
+}
